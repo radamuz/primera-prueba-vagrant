6053b551fbfe26edac976ba6109457b05975fb62
radamuz <radamuzc@gmail.com>
2021-07-08 11:21:22 +0200

    red mvs no funciona

--- 02-17-include-y-roles/comandos.sh
+++ 02-17-include-y-roles/comandos.sh
@@ -1,3 +1,9 @@
 ansible-playbook --syntax-check include.yml
 
 ansible-playbook include.yml -i ../02-12-esenciales/inventarios1/
+
+ansible-playbook copy-apache2-conf-to-localhost.yml -i ../02-12-esenciales/inventarios0/
+
+ansible-playbook --syntax-check instalar.yml
+
+ansible-playbook instalar.yml -i ../02-12-esenciales/inventarios0/
