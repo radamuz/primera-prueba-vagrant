561ccd3c51407ded2c030bf660947268c70fdd08
radamuz <radamuzc@gmail.com>
2021-07-09 13:23:49 +0200

    03-28-modulo-ficheros2

---
+++ 03-28-modulo-ficheros2-y-openssl/comandos.sh
@@ -0,0 +1,23 @@
+
+ansible-playbook modulos_ficheros6.yml -i ../02-12-esenciales/inventarios0/
+ansible-playbook modulos_ficheros7.yml -i ../02-12-esenciales/inventarios0/
+ansible-playbook modulos_ficheros8.yml -i ../02-12-esenciales/inventarios0/
+
+ls /tmp/backup
+
+find /tmp/backup
+
+tree /tmp/backup
+
+ansible-playbook modulos_ficheros9.yml -i ../02-12-esenciales/inventarios0/
+
+cat /tmp/interfaces
+
+ansible-playbook modulos_ficheros10.yml -i ../02-12-esenciales/inventarios0/
+
+ansible-playbook modulos_ficheros11.yml -i ../02-12-esenciales/inventarios0/
+
+ansible-playbook modulos_ficheros12.yml -i ../02-12-esenciales/inventarios0/
+
+ansible-playbook modulos_ficheros13.yml -i ../02-12-esenciales/inventarios0/
+
