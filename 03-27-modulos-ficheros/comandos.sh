9a38722ddda7db2a49a2cbe70884751865460168
radamuz <radamuzc@gmail.com>
2021-07-09 12:00:18 +0200

    03-27-modulos-ficheros

---
+++ 03-27-modulos-ficheros/comandos.sh
@@ -0,0 +1,6 @@
+ansible-playbook modulos_ficheros.yml -i ../02-12-esenciales/inventarios0/
+ansible-playbook modulos_ficheros2.yml -i ../02-12-esenciales/inventarios0/
+ansible-playbook modulos_ficheros3.yml -i ../02-12-esenciales/inventarios0/
+ansible-playbook modulos_ficheros4.yml -i ../02-12-esenciales/inventarios0/
+ansible-playbook modulos_ficheros5.yml -i ../02-12-esenciales/inventarios0/
+
