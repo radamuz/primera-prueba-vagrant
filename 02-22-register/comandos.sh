f48e1c0dfe6428b84510ba940c5494c0a137c208
radamuz <radamuzc@gmail.com>
2021-07-09 09:09:48 +0200

    02-22-register

---
+++ 02-22-register/comandos.sh
@@ -0,0 +1,6 @@
+# Ver la salida del comando uptime con -v
+ansible-playbook register.yml -i ../02-12-esenciales/inventarios0/ -v
+
+ansible-playbook register.yml -i ../02-12-esenciales/inventarios0/
+
+ansible-playbook instalar.yml -i ../02-12-esenciales/inventarios0/
