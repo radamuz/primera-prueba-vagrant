df470c40dd50aa5c4201f9848d121a5ffcb5b122
radamuz <radamuzc@gmail.com>
2021-07-12 19:22:55 +0200

    03-33 1

--- 03-33-modulos-comandos-utilidades/comandos.sh
+++ 03-33-modulos-comandos-utilidades/comandos.sh
@@ -1,1 +1,13 @@
 ansible-playbook crea_script_localmente.yml -i ../02-12-esenciales/inventarios0/
+
+ansible-playbook crea_script_localmente.yml -i ../02-12-esenciales/inventarios0/ -e "pass=dsfdfs"
+
+ansible-playbook ejecuta_script_local_en_remoto.yml -i ../02-12-esenciales/inventarios0/
+
+ansible-playbook ejecuta_script_local_en_remoto.yml -i ../02-12-esenciales/inventarios0/ -e "pass=dsfdfs"
+
+ansible-playbook ejecuta_script_local_en_remoto.yml -i ../02-12-esenciales/inventarios0/ -vv
+
+ansible-playbook ejecuta_script_local_en_remoto.yml -i ../02-12-esenciales/inventarios0/
+
+ansible-playbook modulos_utilidades.yml -i ../02-12-esenciales/inventarios0/
