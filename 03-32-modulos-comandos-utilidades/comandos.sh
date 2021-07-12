2eac631b7cbfba92b9bdc65510bff1262a1b1c88
radamuz <radamuzc@gmail.com>
2021-07-12 15:53:22 +0200

    03-32

---
+++ 03-32-modulos-comandos-utilidades/comandos.sh
@@ -0,0 +1,9 @@
+ansible-playbook modulos_comandos.yml -i ../02-12-esenciales/inventarios0/
+
+ansible-playbook modulos_comandos.yml -i ../02-12-esenciales/inventarios0/ -vv
+
+ansible-playbook modulos_comandos.yml -i ../02-12-esenciales/inventarios0/
+
+ansible-playbook modulos_comandos2.yml -i ../02-12-esenciales/inventarios0/ -vv
+
+ansible-playbook modulos_comandos3.yml -i ../02-12-esenciales/inventarios0/ -vv
