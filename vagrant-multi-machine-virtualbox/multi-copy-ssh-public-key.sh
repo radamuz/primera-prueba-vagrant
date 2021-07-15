03a811b4dcc296c08a636af249459ceb1976f901
radamuz <radamuzc@gmail.com>
2021-07-15 09:17:03 +0200

    script LF

--- vagrant-multi-machine-virtualbox/multi-copy-ssh-public-key.sh
+++ vagrant-multi-machine-virtualbox/multi-copy-ssh-public-key.sh
@@ -1,11 +1,11 @@
+#!/bin/sh
 file="$1"
-lines=$(cat $file)
-file_tr=`cat $file | tr " " ","`
-
-for line in $file_tr;
+# file_tr=$(cat "$file" | tr " " ",")
+file_tr=$(tr " " "," < $file)
+for line in $file_tr
 do
-    ip=`echo $line | cut -d "," -f1`;
-    user=`echo $line | cut -d "," -f2`;
-    pass=`echo $line | cut -d "," -f3`;
-    sshpass -p $pass ssh-copy-id -i ~/.ssh/id_rsa.pub -oStrictHostKeyChecking=no $user@$ip
+    ip=$(echo "$line" | cut -d "," -f1)
+    user=$(echo "$line" | cut -d "," -f2)
+    pass=$(echo "$line" | cut -d "," -f3)
+    sshpass -p "$pass" ssh-copy-id -i ~/.ssh/id_rsa.pub -oStrictHostKeyChecking=no "$user"@"$ip"
 done
