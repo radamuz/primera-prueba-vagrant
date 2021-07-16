c7453c3dda477a2563eadab391442a03c8213f44
radamuz <radamuzc@gmail.com>
2021-07-16 22:20:44 +0200

    comandos instalacion awx

--- ansible-galaxy-awx-installation/comandos.sh
+++ ansible-galaxy-awx-installation/comandos.sh
@@ -1,1 +1,4 @@
+# No es necesario ya que esta colección solo nos permitirá administrar el servicio de awxcon ansible
 ansible-galaxy collection install awx.awx
+
+ansible-galaxy collection install community.general
