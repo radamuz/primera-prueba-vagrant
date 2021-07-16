820cfa34aa5d626cd4dc4290cc1785e028a8b653
radamuz <radamuzc@gmail.com>
2021-07-16 12:39:55 +0200

    remove known_hosts funciona en linux y en windows

--- vagrant-multi-machine-virtualbox/remove-known-hosts-no-funciona.sh
+++ vagrant-multi-machine-virtualbox/remove-known-hosts-no-funciona.sh
@@ -1,9 +1,9 @@
-if [ -d /home ]; 
+if [ "$HOME" == *"/home"* ]; 
 then
     rm "/home/$USER/.ssh/known_hosts"
     echo "known_hosts eliminado en Linux"
-elif [ -d /c/Users ]; then
-# else
+elif [ "$HOME" == *"/c"* ]; 
+then
     rm /c/Users/$USERNAME/.ssh/known_hosts
     echo "known_hosts eliminado en Windows"
 fi
